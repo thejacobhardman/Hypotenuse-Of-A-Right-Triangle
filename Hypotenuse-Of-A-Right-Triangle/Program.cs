9a73a429b86ca22d0493654ea90f4044df2854da
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-10-17 13:54:01 -0600

    add program

--- Hypotenuse-Of-A-Right-Triangle/Program.cs
+++ Hypotenuse-Of-A-Right-Triangle/Program.cs
@@ -10,7 +10,36 @@ namespace Hypotenuse_Of_A_Right_Triangle
     {
         static void Main(string[] args)
         {
+            bool isRunning = true;
 
+            while (isRunning)
+            {
+                double side1, side2;
+
+                Console.Write("Enter Side 1 (Negative to Quit): ");
+                side1 = Convert.ToDouble(Console.ReadLine());
+
+                if (side1 < 0) { break; }
+
+                Console.Write("Enter Side 2 (Negative to Quit): ");
+                side2 = Convert.ToDouble(Console.ReadLine());
+
+                if (side2 < 0) { break; }
+
+                double result = CalculateHyp(side1, side2);
+
+                Console.WriteLine("Hypotenuse is " + result + ".");
+            }
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+
+        static double CalculateHyp(double side1, double side2)
+        {
+            double hyp = Math.Pow(side1, 2) + Math.Pow(side2, 2);
+            hyp = Math.Sqrt(hyp);
+            return hyp;
         }
     }
 }
