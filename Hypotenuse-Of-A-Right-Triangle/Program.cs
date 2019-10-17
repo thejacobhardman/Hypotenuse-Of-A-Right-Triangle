57b2cff4c746a7caa5ce879743e35ee25d7a0f2c
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-10-17 12:58:14 -0600

    Add project files.

---
+++ Hypotenuse-Of-A-Right-Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypotenuse_Of_A_Right_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
